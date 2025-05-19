7a7e8adf40824bdf15d7964b117f84d0bb9a99d9
Geemmeet <ellen.gemback@gmail.com>
2025-05-19 11:32:31 +0200

    Skeleton of Buttons

--- filmlista/src/app/page.js
+++ filmlista/src/app/page.js
@@ -1,13 +1,14 @@
 import Select from '@/components/Select/index'
 import InputField from '@/components/InputField';
+import Button from '@/components/Button';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <h1 className="text-center mt-5">Min filmlista</h1>
-      <form className="container">
+      <form>
         <fieldset>
           <legend>Lägg till en film</legend>
           <hr />
@@ -24,7 +25,22 @@ export default function Home() {
             labelText={'Betyg:'}
           />
         </fieldset>
+        <Button
+          onClick={''} 
+          styleClass={'btn btn-success mt-3'} 
+          text={'Spara'}
+        />
       </form>
+      <Button 
+        onClick={''} 
+        styleClass={'btn btn-primary mt-5 me-2'} 
+        text={'Alfabetisk ordning'}
+      />
+      <Button 
+        onClick={''} 
+        styleClass={'btn btn-primary mt-5'} 
+        text={'Betygsordning'}
+      />
 
     </div>
   );
